ce646e5a208713498797fd769d93324f5f2839aa
Hetal <hmangukia9@gmail.com>
2021-03-23 21:21:12 -0700

    classes added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+
     state = {
-        count: 0
+        count: 0,
     };
+
     render() { 
-        return <React.Fragment><h1>Hello World</h1><button>Increment</button></React.Fragment>;
+        return (
+            <div>
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+            </div>
+        );
     }
+
+    formatCount(){
+        const {count} = this.state;
+        return count === 0 ? "Zero" : count;
+    }
+
 }
  
 export default Counter;
