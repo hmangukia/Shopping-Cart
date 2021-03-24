192939c3b53f8e63b6b5a4a86383ff139f0c795e
Hetal <hmangukia9@gmail.com>
2021-03-23 21:57:31 -0700

    state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,7 +4,7 @@ class Counter extends Component {
 
     state = {
         count: 0,
-        tags: ["tag1", "tag 2", "tag3"]
+        // tags: ["tag1", "tag 2", "tag3"]
     };
 
     constructor(){
@@ -18,7 +18,7 @@ class Counter extends Component {
     }
 
     handleIncrement(){
-        console.log("Increment clicked", this)
+        this.setState({ count: this.state.count + 1 })
     }
 
     render() { 
@@ -26,7 +26,7 @@ class Counter extends Component {
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
-                { this.renderTags() }
+                {/* { this.renderTags() } */}
             </div>
         );
     }
