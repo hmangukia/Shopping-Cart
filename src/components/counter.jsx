a07503b158e6200b0d5120e56fbc10fa001bf95a
Hetal <hmangukia9@gmail.com>
2021-03-25 20:10:24 -0700

    adding arguments in state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,7 +17,7 @@ class Counter extends Component {
         return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
     }
 
-    handleIncrement(){
+    handleIncrement = product => {
         this.setState({ count: this.state.count + 1 })
     }
 
@@ -25,7 +25,10 @@ class Counter extends Component {
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={ () => this.handleIncrement(product)} 
+                    className="btn btn-secondary btn-sm">
+                        Increment
+                </button>
                 {/* { this.renderTags() } */}
             </div>
         );
