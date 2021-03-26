88325cffa315e2d13cdb0bf5521c1a47e24d5221
Hetal <hmangukia9@gmail.com>
2021-03-25 21:29:42 -0700

    adding prop values to state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 
-    state = {
-        count: 0,
-        // tags: ["tag1", "tag 2", "tag3"]
-    };
+    // state = {
+    //     count: this.props.value    
+    // };
 
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        this.state= {
+            value : this.props.value
+        }
         this.handleIncrement = this.handleIncrement.bind(this);
     }
+
+    // constructor(){
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
 
     renderTags(){
         if (this.state.tags.length === 0) return <p>There are no tags!</p>
@@ -18,10 +25,10 @@ class Counter extends Component {
     }
 
     handleIncrement = () => {
-        this.setState({ count: this.state.count + 1 })
+        this.setState({ value: this.state.value + 1 })
     }
 
-    render() { 
+    render() {
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
@@ -36,13 +43,13 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const {count} = this.state;
-        return count === 0 ? "Zero" : count;
+        const {value} = this.state;
+        return value === 0 ? "Zero" : value;
     }
 
 }
